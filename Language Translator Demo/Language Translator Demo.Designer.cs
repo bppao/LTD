2b0366c1e2bcd5fd904002e481502842fe2e6085
Ben <bpp1291@gmail.com>
2013-04-14 16:49:01 -0400

    added all files

---
+++ Language Translator Demo/Language Translator Demo.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Language_Translator_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spanishTB = new System.Windows.Forms.TextBox();
+            this.clearSpanish = new System.Windows.Forms.Button();
+            this.translateButton = new System.Windows.Forms.Button();
+            this.translation = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1220, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearAllToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.clearAllToolStripMenuItem.Text = "&Clear All";
+            this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // spanishTB
+            // 
+            this.spanishTB.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spanishTB.Location = new System.Drawing.Point(349, 318);
+            this.spanishTB.Name = "spanishTB";
+            this.spanishTB.Size = new System.Drawing.Size(500, 32);
+            this.spanishTB.TabIndex = 1;
+            this.spanishTB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.spanishTB_KeyDown);
+            // 
+            // clearSpanish
+            // 
+            this.clearSpanish.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearSpanish.Location = new System.Drawing.Point(855, 318);
+            this.clearSpanish.Name = "clearSpanish";
+            this.clearSpanish.Size = new System.Drawing.Size(75, 33);
+            this.clearSpanish.TabIndex = 2;
+            this.clearSpanish.Text = "Clear";
+            this.clearSpanish.UseVisualStyleBackColor = true;
+            this.clearSpanish.Click += new System.EventHandler(this.clearSpanish_Click);
+            // 
+            // translateButton
+            // 
+            this.translateButton.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translateButton.Location = new System.Drawing.Point(436, 395);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(342, 67);
+            this.translateButton.TabIndex = 5;
+            this.translateButton.Text = "Translate";
+            this.translateButton.UseVisualStyleBackColor = true;
+            this.translateButton.Click += new System.EventHandler(this.translateButton_Click);
+            // 
+            // translation
+            // 
+            this.translation.BackColor = System.Drawing.Color.Transparent;
+            this.translation.Location = new System.Drawing.Point(362, 520);
+            this.translation.Name = "translation";
+            this.translation.Size = new System.Drawing.Size(500, 28);
+            this.translation.TabIndex = 8;
+            this.translation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(12, 820);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Copyright © Ben Paolillo 2013";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Language_Translator_Demo.Properties.Resources.Translator21;
+            this.ClientSize = new System.Drawing.Size(1220, 842);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.translation);
+            this.Controls.Add(this.translateButton);
+            this.Controls.Add(this.clearSpanish);
+            this.Controls.Add(this.spanishTB);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Language Translator Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox spanishTB;
+        private System.Windows.Forms.Button clearSpanish;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+        private System.Windows.Forms.Button translateButton;
+        private System.Windows.Forms.Label translation;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Language Translator Demo/Language Translator Demo.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Language_Translator_Demo
+{
+    public partial class Form1 : Form
+    {
+        // Create a new dictionary to store the Spanish words and their translations
+        Dictionary<string, string> words = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Add the Spanish words and their translations
+            words.Add("Tengo", "I have"); 
+            words.Add("tengo", "I have");
+            words.Add("una", "a"); 
+            words.Add("uno", "a");
+            words.Add("bicicleta", "bicycle"); 
+            words.Add("Bicicleta", "bicycle");
+            words.Add("bicicleta nueva", "new bicycle");
+            words.Add("Bicicleta nueva", "new bicycle");
+            words.Add("Una bicicleta nueva", "A new bicycle");
+            words.Add("una bicicleta nueva", "a new bicycle");
+            words.Add("nueva", "new"); 
+            words.Add("Nueva", "new");
+            words.Add("Tengo una", "I have a");
+            words.Add("tengo una", "I have a");
+            words.Add("Tengo una bicicleta", "I have a bicycle");
+            words.Add("tengo una bicicleta", "I have a bicycle");
+            words.Add("Tengo una bicicleta nueva", "I have a new bicycle");
+            words.Add("tengo una bicicleta nueva", "I have a new bicycle");
+
+            words.Add("Me", "me"); 
+            words.Add("me", "me");
+            words.Add("puede", "can"); 
+            words.Add("comer", "eat");
+            words.Add("ahora", "now");
+            words.Add("¿Me puede comer ahora?", "Can I eat now?");
+            words.Add("¿me puede comer ahora?", "Can I eat now?");
+
+            words.Add("Vamos a ir", "Let's go");
+            words.Add("vamos a ir", "Let's go");
+            words.Add("vamos", "Let's go");
+            words.Add("Vamos", "Let's go");
+            words.Add("al", "to the"); 
+            words.Add("al cine", "the movies");
+            words.Add("mañana", "tomorrow");
+            words.Add("Vamos a ir al cine mañana", "Let's go to the movies tomorrow");
+            words.Add("vamos a ir al cine mañana", "Let's go to the movies tomorrow");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Exit the application here
+            Application.Exit();
+        }
+
+        private void clearSpanish_Click(object sender, EventArgs e)
+        {
+            // Clear the Spanish textbox
+            spanishTB.Text = "";
+        }
+
+        private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Clear both the Spanish textbox and the translation label
+            spanishTB.Text = "";
+            translation.Text = "";
+        }
+
+        private void translateButton_Click(object sender, EventArgs e)
+        {
+            // Check to see if the Spanish word/sentence is in our dictionary
+            // and output the translation
+            string value = "";
+
+            // Error-checking
+            if (spanishTB.Text == "")
+            {
+                MessageBox.Show("Nothing to translate!", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            if (words.TryGetValue(spanishTB.Text, out value))
+            {
+                translation.Font = new System.Drawing.Font("Arial", 18.0f);
+                translation.ForeColor = Color.White;
+                translation.BackColor = Color.Transparent;
+                translation.TextAlign = ContentAlignment.MiddleCenter;
+                translation.Text = value;
+            }
+            else
+            {
+                translation.Text = "";
+
+                MessageBox.Show("No translation found!", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                spanishTB.SelectionStart = 0;
+                spanishTB.SelectionLength = spanishTB.Text.Length;
+            }
+        }
+
+        private void spanishTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Allows the user to hit enter to translate instead of 
+            // having to always click the button
+            if (e.KeyCode == Keys.Enter)
+                translateButton.PerformClick();
+        }
+    }
+}
